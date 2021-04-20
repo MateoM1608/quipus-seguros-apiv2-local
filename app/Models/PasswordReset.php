fce75084306c96022ae0edcac7e1f7899cf563df
Juan Corrales <jdcorraless@gmail.com>
2021-04-20 00:23:56 -0500

    Model for password reset

---
+++ app/Models/PasswordReset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    const UPDATED_AT = null;
+
+    protected $fillable = [
+        'email', 'token'
+    ];
+
+    protected $hidden = [
+        'email', 'token'
+    ];
+}
