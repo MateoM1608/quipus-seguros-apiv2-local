1c3eda3a95874900d88350d6803478d233be1a7e
Juan Corrales <jdcorraless@gmail.com>
2021-04-20 00:19:56 -0500

    Base form request for send responses in json

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Waavi\Sanitizer\Laravel\SanitizesInput;
+use Illuminate\Http\JsonResponse;
+
+abstract class BaseFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize();
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(["errors" => $validator->messages()], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
