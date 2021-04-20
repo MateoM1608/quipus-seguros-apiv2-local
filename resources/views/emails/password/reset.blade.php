332aa3ce0e60a0c85a9d33528c30301a6aed9da4
Juan Corrales <jdcorraless@gmail.com>
2021-04-20 00:17:42 -0500

    Templates for emails when reset password

---
+++ resources/views/emails/password/reset.blade.php
@@ -0,0 +1,40 @@
+<table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_container" style="background-color:#ffffff;border:1px solid #dedede;border-radius:3px">
+    <tbody>
+        <tr>
+            <td align="center" valign="top">
+                <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_header" style="background-color:#8675a0;color:#ffffff;border-bottom:0;font-weight:bold;line-height:100%;vertical-align:middle;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;border-radius:3px 3px 0 0">
+                    <tbody>
+                        <tr>
+                            <td id="m_9139881685967467246header_wrapper" style="padding:36px 48px;display:block">
+                                <h1 style="font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:30px;font-weight:300;line-height:150%;margin:0;text-align:left;color:#ffffff"><span class="il">Solicitud de cambio de contraseña</span> Quipus seguros</h1>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+        <td align="center" valign="top">
+            <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_body">
+                <tbody>
+                    <tr>
+                        <td valign="top" id="m_9139881685967467246body_content" style="background-color:#ffffff">
+                            <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                <tbody>
+                                    <tr>
+                                        <td valign="top" style="padding:48px 48px 32px">
+                                            <div id="m_9139881685967467246body_content_inner" style="color:#636363;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:14px;line-height:150%;text-align:left">
+                                                <p style="margin:0 0 16px">Se ha recibido una solicitud de cambio de contraseña, ingrese al siguiente enlace para terminar el proceso <a href="{{ $url }}/api/v1?token{{ $token }}">cambio de contraseña</a>. De lo contrario haga caso omiso a este correo</p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
---
+++ resources/views/emails/user/create.blade.php
@@ -0,0 +1,48 @@
+<table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_container" style="background-color:#ffffff;border:1px solid #dedede;border-radius:3px">
+    <tbody>
+        <tr>
+            <td align="center" valign="top">
+                <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_header" style="background-color:#8675a0;color:#ffffff;border-bottom:0;font-weight:bold;line-height:100%;vertical-align:middle;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;border-radius:3px 3px 0 0">
+                    <tbody>
+                        <tr>
+                            <td id="m_9139881685967467246header_wrapper" style="padding:36px 48px;display:block">
+                                <h1 style="font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:30px;font-weight:300;line-height:150%;margin:0;text-align:left;color:#ffffff"><span class="il">Bienvenido</span> a Quipus seguros</h1>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        <tr>
+        <td align="center" valign="top">
+            <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_body">
+                <tbody>
+                    <tr>
+                        <td valign="top" id="m_9139881685967467246body_content" style="background-color:#ffffff">
+                            <table border="0" cellpadding="20" cellspacing="0" width="100%">
+                                <tbody>
+                                    <tr>
+                                        <td valign="top" style="padding:48px 48px 32px">
+                                            <div id="m_9139881685967467246body_content_inner" style="color:#636363;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:14px;line-height:150%;text-align:left">
+                                                <p style="margin:0 0 16px">Hola {{ $user }},</p>
+                                                <p style="margin:0 0 16px">Gracias por ser parte de Quipus seguros. Para acceder a nuestro sistema ingresa a <a href="http://localhost:8080/pages/login">http://www.seguros.amauttasystems.com/</a> con los siguientes datos:</p>
+
+                                                <span>Tu correo: <strong>{{ $email }}</strong></span>
+                                                <br>
+                                                <span>Tu contraseña: <strong>{{ $password }}</strong></span>
+                                                <br>
+                                                <br>
+                                                <p style="margin:0 0 16px">Recuerda que puedes cambiar la clave cuando accedas a la aplicación.</p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
