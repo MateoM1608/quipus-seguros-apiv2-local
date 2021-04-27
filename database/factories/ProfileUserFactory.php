d41e3bd8c23635b727b30ed35b209bacaf3de49f
Juan Corrales <jdcorraless@gmail.com>
2021-04-26 22:43:24 -0500

    Changes profile user

---
+++ database/factories/ProfileUserFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProfileUser;
+use Faker\Generator as Faker;
+
+$factory->define(ProfileUser::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
