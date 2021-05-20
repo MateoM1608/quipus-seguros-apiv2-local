d0ab9a81f25711ad20d5ba24b42d4675dc379ec7
Juan Corrales <jdcorraless@gmail.com>
2021-05-20 00:13:49 -0500

    Modificación canal de vendedores

--- app/Events/GVendorEvent.php
+++ app/Events/GVendorEvent.php
@@ -10,6 +10,8 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+use App\Models\Policy\GVendor;
+
 class GVendorEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
@@ -21,7 +23,7 @@ class GVendorEvent implements ShouldBroadcast
      *
      * @return void
      */
-    public function __construct($vendor)
+    public function __construct(GVendor $vendor)
     {
         $this->vendor = $vendor;
     }
