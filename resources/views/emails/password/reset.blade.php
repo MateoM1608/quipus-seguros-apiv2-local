af20148688f9178660aabcdad977140e936f4651
Juan Corrales <jdcorraless@gmail.com>
2021-10-12 23:02:53 -0500

    Cambios en la plantilla de password reset

--- resources/views/emails/password/reset.blade.php
+++ resources/views/emails/password/reset.blade.php
@@ -2,11 +2,11 @@
     <tbody>
         <tr>
             <td align="center" valign="top">
-                <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_header" style="background-color:#8675a0;color:#ffffff;border-bottom:0;font-weight:bold;line-height:100%;vertical-align:middle;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;border-radius:3px 3px 0 0">
+                <table border="0" cellpadding="0" cellspacing="0" width="600" id="m_9139881685967467246template_header" style="background-color:#914BC0;color:#ffffff;border-bottom:0;font-weight:bold;line-height:100%;vertical-align:middle;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;border-radius:3px 3px 0 0">
                     <tbody>
                         <tr>
                             <td id="m_9139881685967467246header_wrapper" style="padding:36px 48px;display:block">
-                                <h1 style="font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:30px;font-weight:300;line-height:150%;margin:0;text-align:left;color:#ffffff"><span class="il">Solicitud de cambio de contraseña</span> Quipus seguros</h1>
+                                <h1 style="font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:30px;font-weight:300;line-height:150%;margin:0;text-align:left;color:#ffffff"><span class="il">Solicitud de recuperación de contraseña</span> Quipus seguros</h1>
                             </td>
                         </tr>
                     </tbody>
@@ -24,7 +24,7 @@
                                     <tr>
                                         <td valign="top" style="padding:48px 48px 32px">
                                             <div id="m_9139881685967467246body_content_inner" style="color:#636363;font-family:&quot;Helvetica Neue&quot;,Helvetica,Roboto,Arial,sans-serif;font-size:14px;line-height:150%;text-align:left">
-                                                <p style="margin:0 0 16px">Se ha recibido una solicitud de cambio de contraseña, ingrese al siguiente enlace para terminar el proceso <a href="{{ $url }}/api/v1?token{{ $token }}">cambio de contraseña</a>. De lo contrario haga caso omiso a este correo</p>
+                                                <p style="margin:0 0 16px;">Se ha recibido una solicitud de recuperación de contraseña, ingrese al siguiente enlace para terminar el proceso <a href="{{ $url }}/api/v1?token{{ $token }}">recuperación de contraseña</a>. De lo contrario haga caso omiso a este correo</p>
                                             </div>
                                         </td>
                                     </tr>
