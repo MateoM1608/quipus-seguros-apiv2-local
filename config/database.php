6298bdb03f8aebe8bd2d87a725ff249bb44ce2e4
Santiago Giraldo Aristizábal <arbolsabio@gmail.com>
2022-10-30 17:35:21 -0500

    Creación nueva instancia local para el sistema de la empresa

--- config/database.php
+++ config/database.php
@@ -142,6 +142,26 @@ return [
             ]) : [],
         ],
 
+        'amautta' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_AMAUTTA', '127.0.0.1'),
+            'port' => env('DB_PORT_AMAUTTA', '3306'),
+            'database' => env('DB_DATABASE_AMAUTTA', 'forge'),
+            'username' => env('DB_USERNAME_AMAUTTA', 'forge'),
+            'password' => env('DB_PASSWORD_AMAUTTA', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         /*--- DB CONNECTION ---*/
     ],
 
